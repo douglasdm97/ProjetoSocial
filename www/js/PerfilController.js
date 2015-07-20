609e5b638329b8ea08fd796d9a41060e027a67cd
Jeandro Couto <jeandro.couto@gmail.com>
2015-07-20 01:14:50 -0300

    Profile Pic

--- www/js/PerfilController.js
+++ www/js/PerfilController.js
@@ -1,7 +1,7 @@
 // Controller Do PErfil
 controllers.
 controller('PerfilController', function($scope, $state, $ionicLoading, $ionicModal, $ionicPopup, $ionicHistory) {
-
+var testefile='' ;
 	Parse.initialize("DGGAyXG486w5hxkzdlX38yqbqfnKb9gywUXGFunJ", "WaKyF5ZAxo9zkieeDENUPxPlXKQd9CBCZtc6VRPW");
 
 
@@ -10,6 +10,14 @@ controller('PerfilController', function($scope, $state, $ionicLoading, $ionicMod
 	$scope.username = User.get("username");
 	$scope.nome = User.get("nome");
 	$scope.email = User.get("email");
+	fotoperfil = User.get("profile_pic");
+	if (!fotoperfil) {
+		$scope.pic_file = "img/no-profile-image.jpg";
+	}else{
+		$scope.pic_file = fotoperfil._url;
+	}
+	
+
 
 	$scope.submit = function (username, nome, email) {
 		User.set('username', username);
@@ -26,6 +34,46 @@ controller('PerfilController', function($scope, $state, $ionicLoading, $ionicMod
 			}
 		});
 	};
+
+
+	$scope.upload_pic_profile = function(){
+		alert();
+	}
+
+    $scope.uploadImage = function () {
+       
+    	var fileUploadControl = $("#pic_file")[0];
+
+    	if (fileUploadControl.files.length > 0) {
+		  var file = fileUploadControl.files[0];
+		  testefile = file;
+		  var name = testefile.name;
+		  
+
+		  var parseFile = new Parse.File(name, file);
+
+		  User.set('profile_pic', parseFile);
+		User.save(null, {
+			success: function (){
+				var alertPopup = $ionicPopup.alert({
+					title: "Foto de Perfil Atualizada!"
+				});
+				alertPopup.then(function(res) {
+					$ionicHistory.goBack();
+				});
+			}
+		});
+
+
+		}
+
+
+
+    }
+
+
+
+
 	$scope.sair = function () {
 		Parse.User.logOut();
 		return $state.go('inicio');
